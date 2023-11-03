d87eea6da07e6c5847c764b1b7205fd5393a0c31
Jules <julesfpollender@gmail.com>
2023-11-03 14:11:49 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Disnat-automation
 Selenium bot to automate Disnat transactions
+
+# First time start
+* Install Python
+* Run `pip install -r requirements.txt`
+* Setup a `.env` file at the project roots. Follow `.env_example` structure
+
+# Start
+* Run `python .\src\main.py`
+
+# Build
+* Run `pyinstaller --onefile src/main.py --name disnat-automation`. `.exe` will be created under `/dist`. Do not forget to set a `.env` file with the `.exe`
---
+++ src/main.py
@@ -0,0 +1,103 @@
+from dotenv import load_dotenv
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+import math
+import logging
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+
+load_dotenv()
+
+browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+wait = WebDriverWait(browser, 3)
+
+def acceptCookies():
+    try: 
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(),"Accept all")]'))).click()
+    except: 
+        return
+
+def userName():
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="popover-login" and .//*[contains(text(),"Log in")]]'))).click()
+
+    elem = wait.until(EC.element_to_be_clickable((By.ID, 'username')))
+    elem.click()
+    elem.send_keys(os.getenv('DISNAT_USER'))
+    elem.send_keys(Keys.RETURN)
+
+def securityQuestion(id, value):
+    questionId = wait.until(EC.presence_of_element_located((By.ID, 'idQuestion'))).get_attribute('value')
+    
+    if questionId != id:
+        return False
+    
+    elem = wait.until(EC.element_to_be_clickable((By.ID, 'answer')))
+    elem.click()
+    elem.send_keys(value)
+    elem.send_keys(Keys.RETURN)
+    return True
+
+def password():
+    elem = wait.until(EC.element_to_be_clickable((By.ID, 'j_password')))
+    elem.click()
+    elem.send_keys(os.getenv('DISNAT_PASSWORD'))
+    elem.send_keys(Keys.RETURN)
+
+def trade():
+    select = Select(wait.until(EC.element_to_be_clickable((By.ID, 'trade-account'))))
+    select.select_by_visible_text(os.getenv('DISNAT_FOLIO_NAME'))
+
+    select = Select(wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'select[id^="trade-transaction"]'))))
+    select.select_by_visible_text('Buy')
+
+    elem = wait.until(EC.element_to_be_clickable((By.ID, 'trade-symbol')))
+    elem.click()
+    elem.send_keys(os.getenv('DISNAT_SYMBOL_CODE'))
+    time.sleep(1)
+    elem.send_keys(Keys.RETURN)
+
+    available = wait.until(EC.presence_of_element_located((By.XPATH, '//*[starts-with(@data-test-field,"buy_power_")]'))).get_attribute("data-test-value")
+    currentPrice = wait.until(EC.presence_of_element_located((By.XPATH, '//span[@domid="LastPrice"]'))).text
+
+    qty = math.floor(float(available) * float(os.getenv('AMOUNT_PERCENTAGE_TO_PLACE')) / float(currentPrice))
+
+    elem = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[id^="trade-quantity"]')))
+    elem.click()
+    elem.send_keys(qty)
+
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//label[contains(text(),"Market")]'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(),"Verify")]'))).click()
+    try:
+        wait.until(EC.element_to_be_clickable((By.XPATH, '//*[contains(text(),"Confirmation : ")]'))).click()
+    except:
+        logging.exception('No confirmation required')
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@data-test-button="submit"]]'))).click()
+    
+
+try: 
+    browser.get('https://www.disnat.com/en')
+
+    acceptCookies()
+    userName()
+    if not securityQuestion(os.getenv('DISNAT_SECURITY_QUESTION_ID1'), os.getenv('DISNAT_SECURITY_QUESTION_RESPONSE1')):
+        if not securityQuestion(os.getenv('DISNAT_SECURITY_QUESTION_ID2'), os.getenv('DISNAT_SECURITY_QUESTION_RESPONSE2')):
+            securityQuestion(os.getenv('DISNAT_SECURITY_QUESTION_ID3'), os.getenv('DISNAT_SECURITY_QUESTION_RESPONSE3'))
+
+    acceptCookies()
+    password()
+
+    # Does not work for some reason...
+    # wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="site-header" and .//a[@data-menu-item="orders"]]'))).click()
+    browser.get('https://tmw.secure.vmd.ca/s9web/secure/orders')
+    time.sleep(1)
+    
+    trade()
+    time.sleep(10)
+finally:
+    browser.quit()
